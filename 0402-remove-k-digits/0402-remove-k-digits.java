00e305579ef9d1a9aa8fd275f118beae07f23c74
Lovepreet Singh <lovepreet270406@gmail.com>
2026-05-16 23:12:02 +0530

    12 ms (84.98%) 46.3 MB (91.24%) 0402-remove-k-digits Medium .java String

---
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        
+        if(num.length() == k) return "0";
+
+        Deque<Character> stk = new ArrayDeque<>();
+
+        for (char c : num.toCharArray()) {
+
+            if(stk.size() == 1 && stk.peek() == '0') stk.pop();
+                
+            while(!stk.isEmpty() && k > 0 && stk.peek() > c){
+                stk.pop();
+                k--;
+            }
+            if(stk.isEmpty() && c =='0') continue;
+
+            stk.push(c);
+        }
+
+        while(!stk.isEmpty() && k > 0){
+            stk.pop();
+            k--;
+        }
+
+        char[] arr = new char[stk.size()];
+        int i = stk.size() - 1;
+        for (char c : stk) {
+            arr[i] = c;
+            i--;
+        }
+
+        String ans = new String(arr);
+        if(ans.length() == 0) return "0";
+        return ans;
+    }
+}
