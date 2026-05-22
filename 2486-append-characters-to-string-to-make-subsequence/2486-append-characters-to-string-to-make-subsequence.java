dea92d52074bca3a088e22ab5126326d9042212d
Lovepreet Singh <lovepreet270406@gmail.com>
2026-05-22 17:48:26 +0530

    7 ms (60.1%) 46.9 MB (87.15%) 2486-append-characters-to-string-to-make-subsequence Medium .java Two Pointers

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -1,16 +1,14 @@
 class Solution {
     public int appendCharacters(String s, String t) {
-        
-        char[] st = s.toCharArray();
-        char[] tt = t.toCharArray();
         int i;
         int j = 0;
 
-        for(i = 0; i < st.length && j < tt.length;i++){
-            if(st[i] == tt[j]){
+        for(i = 0; i < s.length() && j < t.length();i++){
+            if(s.charAt(i) == t.charAt(j)){
                 j++;
             }
         }
-        return (tt.length - j);
+
+        return (t.length() - j);
     }
 }
