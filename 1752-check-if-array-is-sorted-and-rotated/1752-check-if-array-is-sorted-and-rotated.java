fdb52beb530f4fbde9efa73a30984b42a0666107
Lovepreet Singh <lovepreet270406@gmail.com>
2026-05-23 18:39:56 +0530

    0 ms (100%) 42.4 MB (99.26%) 1752-check-if-array-is-sorted-and-rotated Easy .java Array

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -8,13 +8,16 @@ class Solution {
                 break;
             }
         }
-        
+
+        //check for if array is already fully sorted so it is rotated 0 times.
         if(p==nums.length) return true;
 
+        //check for if array is sorted after breaking point
         for(int i = p + 1;i < nums.length - 1;i++){
             if(nums[i] > nums[i+1]) return false;
         }
         
+        //check for is array is valid before rotation by connecting it to first index
         for(int i = p+1;i < nums.length;i++){
             if(nums[0] < nums[i]) return false;
         }
