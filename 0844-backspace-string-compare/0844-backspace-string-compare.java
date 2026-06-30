039515be13ad469ecbd0dd4bd990495fd1712b9a
Lovepreet Singh <lovepreet270406@gmail.com>
2026-06-30 19:46:52 +0530

    2 ms (55.95%) 43.2 MB (37.48%) 0844-backspace-string-compare Easy .java Two Pointers

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    String check(String s){
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char a: s.toCharArray()){
+
+            if(stack.isEmpty() && a=='#') continue;
+
+            if(!stack.isEmpty() && a=='#'){
+                stack.pop();
+            }
+            else stack.push(a);
+        }
+        StringBuilder ans = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            ans.insert(0, stack.pop());
+        }
+        return ans.toString();
+    }
+
+    public boolean backspaceCompare(String s, String t) {
+        String st = check(s);
+        String tt = check(t);
+
+        return (st.equals(tt));
+    }
+}
